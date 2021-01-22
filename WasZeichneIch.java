f7fec371190a0d5ca8fc27568c53437996a48efb
Benginur <benginurzor@gmail.com>
2021-01-22 20:03:23 +0100

    Bild A von Bengi

--- WasZeichneIch.java
+++ WasZeichneIch.java
@@ -23,12 +23,9 @@ public class WasZeichneIch extends PApplet
     // Überlege dir, was die Anweisungen in der Methode "wasZeichneIch" auslösen könnten
     // Verändere die Werte, die den Methoden übergeben werden. Was passiert?
     public void wasZeichneIch() {
-        ellipse(100,95,180,180);
-        rect(10,230,180, 180);
-        ellipse(100,320,150,150);
-        rect(44,42,110,110);
-        rect(50,270,100,100);
-        ellipse(99,98,100,100);
+        ellipse(100,95,130,130);
+        rect(35,160,130,130);
+        triangle(30,400,170,400,97,290);
     }
 
     /**
